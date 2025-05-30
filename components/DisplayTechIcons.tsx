1f90693b78f56e4498935f1c9b314e99ddaf535f
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-05-30 17:45:05 +0300

    feat: implement DisplayTechIcons component to fetch and display technology logos

--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -1,7 +1,28 @@
+import { getTechLogos } from "@/lib/utils";
 import React from "react";
+import Image from "next/image";
 
-const DisplayTechIcons = ({ techStack }: TechIconProps) => {
-  return <div>{techStack}</div>;
+const DisplayTechIcons = async ({ techStack }: TechIconProps) => {
+  const techIcons = await getTechLogos(techStack);
+  return (
+    <div className="flex flex-row">
+      {techIcons.slice(0, 3).map(({ tech, url }, index) => (
+        <div
+          className="relative group bg-dark-300 rounded-full p-2 flex-center"
+          key={tech}
+        >
+          <span className="tech-tooltip">{tech}</span>
+          <Image
+            src={url}
+            alt={tech}
+            width={100}
+            height={100}
+            className="size-5"
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default DisplayTechIcons;
