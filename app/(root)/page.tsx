0c366cac2ce22f1060541ecfc4e34e64f742d9f6
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-05-30 15:09:53 +0300

    feat: implement homepage layout with AI interview preparation section and user interviews display

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,38 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
-  return <div>HomePag asfgsdge</div>;
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice in real interview questions and get instant feedback.
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="robo dude"
+          width={400}
+          height={400}
+          className="w-[300px] h-auto max-sm:hidden"
+          priority
+        />
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          <p>You haven`t taken any interviews yet.</p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default page;
