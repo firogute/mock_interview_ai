107ee51fb54d6f0191b7edf530c17f5f903bccc1
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-05 05:12:47 +0300

    feat: enhance DisplayTechIcons component with conditional class names for better layout

--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -1,4 +1,4 @@
-import { getTechLogos } from "@/lib/utils";
+import { cn, getTechLogos } from "@/lib/utils";
 import React from "react";
 import Image from "next/image";
 
@@ -8,7 +8,7 @@ const DisplayTechIcons = async ({ techStack }: TechIconProps) => {
     <div className="flex flex-row">
       {techIcons.slice(0, 3).map(({ tech, url }, index) => (
         <div
-          className="relative group bg-dark-300 rounded-full p-2 flex-center"
+          className={cn("relative group bg-dark-300 rounded-full p-2 flex-center", index>=1 && "-ml-3" )}
           key={tech}
         >
           <span className="tech-tooltip">{tech}</span>
