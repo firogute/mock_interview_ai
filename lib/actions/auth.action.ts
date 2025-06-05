e069f174937998237eb50d2f4d65f17f09e76601
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-05 17:11:40 +0300

    feat: add signIn and setSessionCookie functions for user authentication

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,5 +1,9 @@
-import { db } from "@/firebase/admin";
+import { Path } from "react-hook-form";
+import { auth, db } from "@/firebase/admin";
 import { error } from "console";
+import { cookies } from "next/headers";
+
+const ONE_WEEK = 60 * 60 * 24 * 7 * 1000;
 
 export async function signUp(params: SignUpParams) {
   const { uid, email, password } = params;
@@ -30,4 +34,47 @@ export async function signUp(params: SignUpParams) {
       message: "An error occurred during sign up. Please try again later.",
     };
   }
+}
+
+export async function signIn(params: SignInParams) {
+  const { email, idToken } = params;
+
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+    if (!userRecord) {
+      return {
+        success: false,
+        message: "No user found with this email.",
+      };
+    }
+
+    await setSessionCookie(idToken);
+    return {
+      success: true,
+      message: "Sign in successful!",
+    };
+  } catch (error: any) {
+    console.error("Error signing in:", error);
+
+    return {
+      success: false,
+      message: "An error occurred during sign in. Please try again later.",
+    };
+  }
+}
+
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: ONE_WEEK,
+  });
+
+  cookieStore.set("session", sessionCookie, {
+    maxAge: ONE_WEEK,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
 }
