280d4135cc942f3cd99364016c83b8cc16f39331
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-06 06:50:22 +0300

    feat: enhance authentication flow by implementing user session verification and redirect logic

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -84,3 +84,30 @@ export async function signIn(params: SignInParams) {
   }
 }
 
+
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+  const sessionCookie = cookieStore.get("session")?.value;
+  if (!sessionCookie) return null;
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+    const userRecord = await db.collection("users").doc(decodedClaims.uid).get();
+    if (!userRecord.exists) {
+      return null;
+    }
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  }catch (error) {
+    console.error("Error verifying session cookie:", error);
+    return null;
+  }
+}
+ 
+export async function isAuthenticated(): Promise<boolean> { 
+  const user = await getCurrentUser();
+
+  return !!user;
+}
