1dff3063324d56625ca91c81022c9899467f39c6
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-07 05:43:13 +0300

    feat: add Agent component with call status handling and user display

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from '@/components/Agent';
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+          <h3>Firomsa Guteta shit is going to happen because of me.</h3>
+          <Agent userName="You" userId="user1"  type="generate"/>
+    </>
+  );
+}
+
+export default page
